16fdab9690733304dba751f1df3110a3b501f55c
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-06-26 19:19:34 +0500

    users signup

--- src/scripts/signup.js
+++ src/scripts/signup.js
@@ -1,1 +1,23 @@
 import '../styles/signup.css'
+
+const form = document.forms.signup;
+
+const URL = 'http://localhost:3000/users';
+
+form.onsubmit = (e) => {
+	e.preventDefault();
+
+	const user = {};
+
+	const fm = new FormData(form);
+
+	fm.forEach((value, key) => {
+		user[key] = value;
+	});
+
+	// create user
+	fetch(URL, {
+		method: 'POST',
+		body: JSON.stringify(user)
+	});
+}
