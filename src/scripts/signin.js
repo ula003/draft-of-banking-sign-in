26a5388df5aa6f48f0648dfef0e36bdc52542ab8
ula003 <xuyilajob.ru@gmail.com>
2024-06-27 20:24:51 +0500

    first commit

--- src/scripts/signin.js
+++ src/scripts/signin.js
@@ -1,3 +1,30 @@
 import '../styles/signin.css'
 
-console.log('asdasd');
+const signin = document.forms.signin;
+const URL = 'http://localhost:3000/users';
+
+function auth (user) {
+    const users = fetch(URL)
+	.then(response => response.json())
+	.then(json => return.json());
+    return Boolean(users.find((elem) => elem.email === user.email && elem.password === user.password));
+}
+signin.onsubmit = (e) => {
+	e.preventDefault();
+
+	const user = {};
+
+	const fm = new FormData(signin);
+
+	fm.forEach((value, key) => {
+		user[key] = value;
+	});
+
+	const isSignedUp = auth(user);
+
+	if (isSignedUp) {
+		alert('Welcome');
+	} else {
+		alert('You should be signed up');
+	}
+}
